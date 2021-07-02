a2f4002b25019afc952f0394c276b1ccacc21363
Dexter <a.esselink@gmail.com>
2021-07-02 14:50:06 +0200

    Fix down scripts

--- migrations/1625155190988_add_tag/down.sql
+++ migrations/1625155190988_add_tag/down.sql
@@ -1,9 +1,3 @@
 -- Could not auto-generate a down migration.
 -- Please write an appropriate down migration for the SQL below:
--- CREATE TABLE "tag" (
-    name VARCHAR NOT NULL,
-    media_id UUID NOT NULL REFERENCES public.media(id),
-    score INT DEFAULT 0,
-    
-    PRIMARY KEY (name, media_id)
-);
+DROP TABLE "tag" CASCADE;
--- migrations/1625155302847_add_comment/down.sql
+++ migrations/1625155302847_add_comment/down.sql
@@ -1,13 +1,3 @@
 -- Could not auto-generate a down migration.
 -- Please write an appropriate down migration for the SQL below:
--- CREATE TABLE "comment" (
-    id UUID NOT NULL DEFAULT uuid_generate_v4 (),
-    text VARCHAR NOT NULL,
-    media_id UUID NOT NULL REFERENCES public.media(id),
-    media_timestamp VARCHAR,
-    
-    user_id UUID NOT NULL REFERENCES public.user(id),
-    created  TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-
-    PRIMARY KEY (id)
-);
+DROP TABLE "comment" CASCADE;

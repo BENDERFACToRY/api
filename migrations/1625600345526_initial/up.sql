6847b7bdd131a5b2cc5c29055b76a6c760af2819
Dexter <a.esselink@gmail.com>
2021-07-06 22:30:52 +0200

    Enabling console

--- migrations/1625600345526_initial/up.sql
+++ migrations/1625600345526_initial/up.sql
@@ -1,4 +1,3 @@
-
 SET check_function_bodies = false;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
@@ -10,9 +9,23 @@ CREATE TABLE "user" (
     PRIMARY KEY (id)
 );
 
+ALTER TABLE public.user ADD column "discord_id" varchar null;
+ALTER TABLE public.user ADD constraint "user_discord_id_key" unique ("discord_id");
+ALTER TABLE public.user ADD constraint "user_name_key" unique ("name");
+
+
 CREATE TABLE media (
     id      UUID NOT NULL default uuid_generate_v4(),
-    ipfs    VARCHAR NOT NULL,
+
+    title VARCHAR NOT NULL,
+    data_folder VARCHAR NOT NULL UNIQUE,
+    stereo_mix JSONB NOT NULL,
+    recorded_date DATE NULL,
+    torrent VARCHAR NULL,
+    tracks JSONB NOT NULL DEFAULT '[]',
+    bpm VARCHAR NULL,
+    youtube_url VARCHAR NULL,
+    season VARCHAR NULL,
 
     PRIMARY KEY (id)
 );
@@ -44,3 +57,16 @@ CREATE TABLE "comment" (
 
     PRIMARY KEY (id)
 );
+
+
+CREATE SCHEMA "oauth";
+
+CREATE TABLE oauth.token (
+    expires TIMESTAMPTZ NOT NULL,
+    access_token VARCHAR NOT NULL,
+    refresh_token VARCHAR,
+    scope JSONB DEFAULT '[]',
+    user_id UUID NOT NULL REFERENCES public.user(id)
+);
+
+
